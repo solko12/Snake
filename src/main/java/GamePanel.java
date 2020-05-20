f62338728d7c0c4c8189e7d414d21c6caead9e97
Oskar <podgrzyb123@gmail.com>
2020-05-20 23:42:20 +0200

    window xd

---
+++ src/main/java/GamePanel.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class GamePanel extends JPanel {
+    public static final int WIDTH = 500, HEIGHT = 500;
+
+    public GamePanel()
+    {
+        setPreferredSize(new Dimension(WIDTH,HEIGHT));
+    }
+    public void start()
+    {
+
+    }
+    public void stop()
+    {
+        
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import javax.swing.*;
+
 public class Main {
+    public Main()
+    {
+        JFrame frame = new JFrame();
+        GamePanel gamePanel = new GamePanel();
+        frame.add(gamePanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Snake");
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        new Main();
     }
 }
